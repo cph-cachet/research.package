cad3a92bd0c9336709c8de69198cc3282b060862
bardram <jakba@dtu.dk>
2021-12-26 11:25:56 +0100

    more fr debugging

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -42,9 +42,7 @@ class MyApp extends StatelessWidget {
       localeResolutionCallback: (locale, supportedLocales) {
         // Check if the current device locale is supported
         for (var supportedLocale in supportedLocales) {
-          if (supportedLocale.languageCode == locale!.languageCode
-              /*  && supportedLocale.countryCode == locale.countryCode */
-              ) {
+          if (supportedLocale.languageCode == locale!.languageCode) {
             return supportedLocale;
           }
         }
