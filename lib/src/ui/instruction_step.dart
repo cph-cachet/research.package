a4698795ac4a256f6b39ea63fff6a29bea0f7dd2
Aamir Farooq <aamir@farooq.xyz>
2024-06-06 15:23:55 +0200

    Remove `simple_html_css` which is unused and causes build issues on Flutter 3.22

--- lib/src/ui/instruction_step.dart
+++ lib/src/ui/instruction_step.dart
@@ -166,22 +166,3 @@ class InstructionImage extends StatelessWidget {
     );
   }
 }
-
-// Render the title above the questionBody
-class InstructionText extends StatelessWidget {
-  final String text;
-  const InstructionText(this.text, {super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    if (text.contains('</')) {
-      return HTML.toRichText(context, text);
-    } else {
-      return Text(
-        text,
-        style: Theme.of(context).textTheme.bodyLarge,
-        textAlign: TextAlign.start,
-      );
-    }
-  }
-}
