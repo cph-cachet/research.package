42e14fbddaf1891d4939f3baecd034b2596a3865
Panagiotis Giannoutsos <36935711+Panosfunk@users.noreply.github.com>
2024-10-02 14:06:32 +0200

    removing commented out code

--- lib/src/ui/instruction_step.dart
+++ lib/src/ui/instruction_step.dart
@@ -388,66 +388,6 @@ class _VideoAppState extends State<VideoApp> {
   }
 }
 
-/// Stateful widget to fetch and then display video content.
-// class VideoApp extends StatefulWidget {
-//   final RPInstructionStep step;
-
-//   const VideoApp({super.key, required this.step});
-
-//   @override
-//   _VideoAppState createState() => _VideoAppState();
-// }
-
-// class _VideoAppState extends State<VideoApp> {
-//   late VideoPlayerController _controller;
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     _controller =
-//         VideoPlayerController.networkUrl(Uri.parse(widget.step.videoPath!))
-//           ..initialize().then((_) {
-//             // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
-//             setState(() {});
-//           });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Video Demo',
-//       home: Scaffold(
-//         body: Center(
-//           child: _controller.value.isInitialized
-//               ? AspectRatio(
-//                   aspectRatio: _controller.value.aspectRatio,
-//                   child: VideoPlayer(_controller),
-//                 )
-//               : Container(),
-//         ),
-//         floatingActionButton: FloatingActionButton(
-//           onPressed: () {
-//             setState(() {
-//               _controller.value.isPlaying
-//                   ? _controller.pause()
-//                   : _controller.play();
-//             });
-//           },
-//           child: Icon(
-//             _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-
-//   @override
-//   void dispose() {
-//     _controller.dispose();
-//     super.dispose();
-//   }
-// }
-
 class AudioPlayerWidget extends StatefulWidget {
   final AudioPlayer player;
 
